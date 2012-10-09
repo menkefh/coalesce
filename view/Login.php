3f59413f2a8505946cf946cd42fb05abab019dfc
menkefh <menkefh@gmail.com>
2012-10-09 17:32:21 -0400

    Added style

--- view/Login.php
+++ view/Login.php
@@ -1,21 +1,24 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="simple.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
 <title>Insert title here</title>
 </head>
 <frameset>
     <frame>
-    <frame>
+    <body>
+    <div>
+    <p>This page uses frames. The current browser you are using does not support frames.</p>
+    </div>
+    <?php
+	echo $_GET['trial'];
+	?>
+    </body>
+    </frame>
     <noframes>
     <body>
     <p>This page uses frames. The current browser you are using does not support frames.</p>
-    <?php
-	foreach($HTTP_GET_VARS as $someData)
-	{
-		echo $someData;
-	}
-	?>
     </body>
     </noframes>
 </frameset>
